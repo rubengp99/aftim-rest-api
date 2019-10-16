d3273bbb537883f62fb40c6ebac590d3ff817df2
= <=>
2019-10-16 11:40:53 -0400

    conceptos

---
+++ src/components/conceptos/controller.ts
@@ -0,0 +1,10 @@
+import * as conceptos from 'helpers/consult';
+import * as links from 'helpers/links'
+import {Request} from 'express';
+import {IConcepto} from './model';
+
+const model = 'conceptos';
+
+export const get = async (req:Request):Promise<any> =>{
+    
+}
