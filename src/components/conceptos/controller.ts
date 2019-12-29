3077035f4d7f169bf66fe91a0f9d9fd573f05c7c
Duccem <josemanuel376@hotmail.com>
2019-12-29 13:00:47 -0300

    Handling errors on conceptos

--- src/components/conceptos/controller.ts
+++ src/components/conceptos/controller.ts
@@ -1,5 +1,6 @@
 import * as conceptos from '../../helpers/consult';
-import * as links from '../../helpers/links'
+import * as links from '../../helpers/links';
+import * as respuestas from '../../errors';
 import { IConcepto } from './model';
 
 const model = 'conceptos';
@@ -7,8 +8,8 @@ const model = 'conceptos';
 /**
  * Get all last concepts
  * @param query modifier of the consult
- */ 
-export const get = async (query:any): Promise<any> => {
+ */
+export const get = async (query: any): Promise<any> => {
     try {
         let data: IConcepto[] = await conceptos.get(model, query);// consulto los conceptos
         let totalCount: number = await conceptos.count(model); // consulto el total de registros de la BD
@@ -16,30 +17,23 @@ export const get = async (query:any): Promise<any> => {
         let { fields, limit } = query;
 
         // si se encontraron registros
-        if (count > 0) {
-            // si no me pasaron campos requeridos o si en los campos estan las presentaciones entonces
-            // consulto las presentaciones de ese producto
-            if (!fields) {
-                for (let i = 0; i < data.length; i++) {
-                    let { id } = data[i];
-                    let pres = await conceptos.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
-                    data[i].presentaciones = pres;
-                }
-            } else if (fields.includes('presentaciones')) {
-                for (let i = 0; i < data.length; i++) {
-                    let { id } = data[i];
-                    let pres = await conceptos.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
-                    data[i].presentaciones = pres; // populo el objeto con el arreglo de presentaciones
-                }
+        if (count <= 0) return respuestas.Empty;
+        // si no me pasaron campos requeridos o si en los campos estan las presentaciones entonces
+        // consulto las presentaciones de ese producto
+        if (!fields || fields.includes('presentaciones')) {
+            for (let i = 0; i < data.length; i++) {
+                let { id } = data[i];
+                let pres = await conceptos.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
+                data[i].presentaciones = pres;
             }
-            let link = links.pages(data, 'conceptos', count, totalCount, limit);
-            let response = Object.assign({ totalCount, count, data }, link);
-            return response;
-        } else {
-            return { message: "No se encontraron registros" }
         }
+        let link = links.pages(data, model, count, totalCount, limit);
+        let response = Object.assign({ totalCount, count, data }, link);
+        return { response, code: respuestas.Ok.code };
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error en el controlador ${model}, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -48,30 +42,27 @@ export const get = async (query:any): Promise<any> => {
  * @param id id of the concept
  * @param query modifier of the consult
  */
-export const getOne = async (id:string | number ,query:any):Promise<any> =>{
+export const getOne = async (id: string | number, query: any): Promise<any> => {
     try {
-        if(isNaN(id as number)){
-            return {message:`${id} no es un ID valido`};
-        }
-        let data:IConcepto[] = await conceptos.getOne(model,id,query);
+        if (isNaN(id as number)) return respuestas.InvalidID;
+
+        let data: IConcepto = await conceptos.getOne(model, id, query);
         let count = await conceptos.count(model);
-        let {fields} = query;
-        if(data[0]){
-            if (!fields) {                
-                let pres = await conceptos.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
-                data[0].presentaciones = pres;
-            } else if (fields.includes('presentaciones')) {
-                let pres = await conceptos.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
-                data[0].presentaciones = pres;
-            }
-            let link = links.records(data,'grupos',count);
-            let response = Object.assign({data},link);
-            return response;
-        }else{
-            return {message:"No se encontro el recurso indicado"};
+        let { fields } = query;
+
+        if (!data) return respuestas.ElementNotFound;
+
+        if (!fields || fields.includes('presentaciones')) {
+            let pres = await conceptos.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
+            data.presentaciones = pres;
         }
+        let link = links.records(data, model, count);
+        let response = Object.assign({ data }, link);
+        return { response, code: respuestas.Ok.code };
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error en el controlador ${model}, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -80,29 +71,29 @@ export const getOne = async (id:string | number ,query:any):Promise<any> =>{
  * @param id id of the concept
  * @param query modifier of the consult
  */
-export const getDepositsByConcept = async (id:string | number,query:any):Promise<any>=>{
+export const getDepositsByConcept = async (id: string | number, query: any): Promise<any> => {
     try {
-        if(isNaN(id as number)){
-            return {message:`${id} no es un ID valido`};
-        }
-        let recurso:IConcepto = await conceptos.getOne(model,id,{fields:'id'});
-        if(!recurso){
-            return {response:{message:"No se encontro el recurso indicado"}, code:404};
-        }
-        let data:any = await conceptos.getOtherByMe(model,id,'movimiento_deposito',{fields:'depositos_id,existencia'});
+        if (isNaN(id as number)) return respuestas.InvalidID;
+
+        let recurso: IConcepto = await conceptos.getOne(model, id, { fields: 'id' });
+
+        if (!recurso) return respuestas.ElementNotFound;
+
+        let data: any = await conceptos.getOtherByMe(model, id, 'movimiento_deposito', { fields: 'depositos_id,existencia' });
         let totalCount = await conceptos.count('depositos');
         let count = data.length;
-        let {limit} = query;
-        if(count > 0){
-            let link = links.pages(data,`conceptos/${id}/depositos`,count,totalCount,limit);
-            let response = Object.assign({totalCount,count,data},link);
-            return {response,code:200};
-        }else{
-            return {response:{count,totalCount,message:"No se encontraron registros"},code:200};
-        }
+        let { limit } = query;
+
+        if (count <= 0) return respuestas.Empty;
+
+        let link = links.pages(data, `conceptos/${id}/depositos`, count, totalCount, limit);
+        let response = Object.assign({ totalCount, count, data }, link);
+        return { response, code: respuestas.Ok.code };
 
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error en el controlador ${model}, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -111,28 +102,25 @@ export const getDepositsByConcept = async (id:string | number,query:any):Promise
  * @param id id of the concept
  * @param query modifier of the consult
  */
-export const getPhotosByConcept = async (id:string | number ,query:any ):Promise<any> => {
+export const getPhotosByConcept = async (id: string | number, query: any): Promise<any> => {
     try {
-        if(isNaN(id as number)){
-            return {message:`${id} no es un ID valido`};
-        }
-        let recurso:IConcepto = await conceptos.getOne(model,id,{fields:'id'});
-        if(!recurso){
-            return {response:{message:"No se encontro el recurso indicado"}, code:404};
-        }
-        let data:any = await conceptos.getOtherByMe(model,id,'rest_galeria',query);
-        let totalCount = await conceptos.countOther(model,'rest_galeria',id);
+        if (isNaN(id as number)) return respuestas.InvalidID;
+
+        let recurso: IConcepto = await conceptos.getOne(model, id, { fields: 'id' });
+        if (!recurso) return respuestas.ElementNotFound;
+
+        let data: any = await conceptos.getOtherByMe(model, id, 'rest_galeria', query);
+        let totalCount = await conceptos.countOther(model, 'rest_galeria', id);
         let count = data.length;
-        let {limit} = query;
-        if(count > 0){
-            let link = links.pages(data,`conceptos/${id}/photos`,count,totalCount,limit);
-            let response = Object.assign({totalCount,count,data},link);
-            return {response,code:200};
-        }else{
-            return {response:{count,totalCount,message:"No se encontraron registros"},code:200};
-        }
+        let { limit } = query;
+        if (count <= 0) return respuestas.Empty;
+        let link = links.pages(data, `conceptos/${id}/photos`, count, totalCount, limit);
+        let response = Object.assign({ totalCount, count, data }, link);
+        return { response, code: respuestas.Ok.code };
     } catch (error) {
-        throw new Error(`Error al consultar la base de datos, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error en el controlador ${model}, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -141,21 +129,23 @@ export const getPhotosByConcept = async (id:string | number ,query:any ):Promise
  * Create a new concept
  * @param body data of the concept
  */
-export const create = async (body:any):Promise<any> =>{
-    let {data,data1} = body;
-    let newConcepto:IConcepto = data;
+export const create = async (body: any): Promise<any> => {
+    let { data, data1 } = body;
+    let newConcepto: IConcepto = data;
     let presentaciones = data1;
     try {
-        let {insertId} = await conceptos.create(model,newConcepto) as any;
+        let { insertId } = await conceptos.create(model, newConcepto) as any;
         presentaciones.forEach(async (element: any) => {
-            element.conceptos_id=insertId;
-            await conceptos.create('presentaciones',element);
+            element.conceptos_id = insertId;
+            await conceptos.create('presentaciones', element);
         });
-        let link = links.created('conceptos',insertId);
-        let response = Object.assign({message:"Registro insertado en la base de datos"},{link:link});
-        return {response,code:201};
+        let link = links.created(model, insertId);
+        let response = Object.assign({ message: respuestas.Created.message }, { link: link });
+        return { response, code: respuestas.Created.code };
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error en el controlador ${model}, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 /**
@@ -163,21 +153,25 @@ export const create = async (body:any):Promise<any> =>{
  * @param params params request object
  * @param query data of the concept
  */
-export const update = async (params:any,body:any):Promise<any> => {
-    let {id} = params;
-    let {data,data1} = body;
-    let newGrupo:IConcepto = data;
+export const update = async (params: any, body: any): Promise<any> => {
+    let { id } = params;
+    let { data, data1 } = body;
+    let newGrupo: IConcepto = data;
     let presentaciones = data1;
     try {
-        let {affectedRows} = await conceptos.update(model,id,newGrupo) as any;
-        presentaciones.forEach(async (element:any) => {
-            await conceptos.update('presentaciones',element.id,element);
+        if (isNaN(id as number)) return respuestas.InvalidID;
+
+        let { affectedRows } = await conceptos.update(model, id, newGrupo) as any;
+        presentaciones.forEach(async (element: any) => {
+            await conceptos.update('presentaciones', element.id, element);
         });
-        let link = links.created('grupos',id);
-        let response = Object.assign({message:"Registro actualizado en la base de datos",affectedRows},{link:link});
-        return {response,code:201};
+        let link = links.created(model, id);
+        let response = Object.assign({ message: respuestas.Update.message, affectedRows }, { link: link });
+        return { response, code: respuestas.Update.code };
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error en el controlador ${model}, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -185,16 +179,19 @@ export const update = async (params:any,body:any):Promise<any> => {
  * Delete a concept
  * @param params params request object
  */
-export const remove = async (params:any):Promise<any> => {
-    let {id} = params;
+export const remove = async (params: any): Promise<any> => {
+    let { id } = params;
     try {
+        if (isNaN(id as number)) return respuestas.InvalidID;
+
         let pres = await conceptos.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
-        pres.forEach(async (element:any) => {
-            await conceptos.remove('presentaciones',element.id);
+        pres.forEach(async (element: any) => {
+            await conceptos.remove('presentaciones', element.id);
         });
-        await conceptos.remove(model,id);
-        return {response:{message:"Registro eliminado de la base de datos"},code:200};   
+        await conceptos.remove(model, id);
+        return respuestas.Deleted;
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        console.log(`Error en el controlador ${model}, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
