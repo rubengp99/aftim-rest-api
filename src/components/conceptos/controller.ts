5500b341045f9c2779562f9dd030b570b05214c4
Duccem <josemanuel376@hotmail.com>
2019-12-29 16:04:30 -0300

    rename of an object

--- src/components/conceptos/controller.ts
+++ src/components/conceptos/controller.ts
@@ -1,4 +1,4 @@
-import * as conceptos from '../../helpers/consult';
+import * as consult from '../../helpers/consult';
 import * as links from '../../helpers/links';
 import * as respuestas from '../../errors';
 import { IConcepto } from './model';
@@ -11,8 +11,8 @@ const model = 'conceptos';
  */
 export const get = async (query: any): Promise<any> => {
     try {
-        let data: IConcepto[] = await conceptos.get(model, query);// consulto los conceptos
-        let totalCount: number = await conceptos.count(model); // consulto el total de registros de la BD
+        let data: IConcepto[] = await consult.get(model, query);// consulto los conceptos
+        let totalCount: number = await consult.count(model); // consulto el total de registros de la BD
         let count = data.length;
         let { fields, limit } = query;
 
@@ -23,7 +23,7 @@ export const get = async (query: any): Promise<any> => {
         if (!fields || fields.includes('presentaciones')) {
             for (let i = 0; i < data.length; i++) {
                 let { id } = data[i];
-                let pres = await conceptos.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
+                let pres = await consult.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
                 data[i].presentaciones = pres;
             }
         }
@@ -46,14 +46,14 @@ export const getOne = async (id: string | number, query: any): Promise<any> => {
     try {
         if (isNaN(id as number)) return respuestas.InvalidID;
 
-        let data: IConcepto = await conceptos.getOne(model, id, query);
-        let count = await conceptos.count(model);
+        let data: IConcepto = await consult.getOne(model, id, query);
+        let count = await consult.count(model);
         let { fields } = query;
 
         if (!data) return respuestas.ElementNotFound;
 
         if (!fields || fields.includes('presentaciones')) {
-            let pres = await conceptos.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
+            let pres = await consult.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
             data.presentaciones = pres;
         }
         let link = links.records(data, model, count);
@@ -75,12 +75,12 @@ export const getDepositsByConcept = async (id: string | number, query: any): Pro
     try {
         if (isNaN(id as number)) return respuestas.InvalidID;
 
-        let recurso: IConcepto = await conceptos.getOne(model, id, { fields: 'id' });
+        let recurso: IConcepto = await consult.getOne(model, id, { fields: 'id' });
 
         if (!recurso) return respuestas.ElementNotFound;
 
-        let data: any = await conceptos.getOtherByMe(model, id, 'movimiento_deposito', { fields: 'depositos_id,existencia' });
-        let totalCount = await conceptos.count('depositos');
+        let data: any = await consult.getOtherByMe(model, id, 'movimiento_deposito', { fields: 'depositos_id,existencia' });
+        let totalCount = await consult.count('depositos');
         let count = data.length;
         let { limit } = query;
 
@@ -106,11 +106,11 @@ export const getPhotosByConcept = async (id: string | number, query: any): Promi
     try {
         if (isNaN(id as number)) return respuestas.InvalidID;
 
-        let recurso: IConcepto = await conceptos.getOne(model, id, { fields: 'id' });
+        let recurso: IConcepto = await consult.getOne(model, id, { fields: 'id' });
         if (!recurso) return respuestas.ElementNotFound;
 
-        let data: any = await conceptos.getOtherByMe(model, id, 'rest_galeria', query);
-        let totalCount = await conceptos.countOther(model, 'rest_galeria', id);
+        let data: any = await consult.getOtherByMe(model, id, 'rest_galeria', query);
+        let totalCount = await consult.countOther(model, 'rest_galeria', id);
         let count = data.length;
         let { limit } = query;
         if (count <= 0) return respuestas.Empty;
@@ -134,10 +134,10 @@ export const create = async (body: any): Promise<any> => {
     let newConcepto: IConcepto = data;
     let presentaciones = data1;
     try {
-        let { insertId } = await conceptos.create(model, newConcepto) as any;
+        let { insertId } = await consult.create(model, newConcepto) as any;
         presentaciones.forEach(async (element: any) => {
             element.conceptos_id = insertId;
-            await conceptos.create('presentaciones', element);
+            await consult.create('presentaciones', element);
         });
         let link = links.created(model, insertId);
         let response = Object.assign({ message: respuestas.Created.message }, { link: link });
@@ -161,9 +161,9 @@ export const update = async (params: any, body: any): Promise<any> => {
     try {
         if (isNaN(id as number)) return respuestas.InvalidID;
 
-        let { affectedRows } = await conceptos.update(model, id, newGrupo) as any;
+        let { affectedRows } = await consult.update(model, id, newGrupo) as any;
         presentaciones.forEach(async (element: any) => {
-            await conceptos.update('presentaciones', element.id, element);
+            await consult.update('presentaciones', element.id, element);
         });
         let link = links.created(model, id);
         let response = Object.assign({ message: respuestas.Update.message, affectedRows }, { link: link });
@@ -184,11 +184,11 @@ export const remove = async (params: any): Promise<any> => {
     try {
         if (isNaN(id as number)) return respuestas.InvalidID;
 
-        let pres = await conceptos.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
+        let pres = await consult.getOtherByMe(model, id as string, 'presentaciones', {}) as any[];
         pres.forEach(async (element: any) => {
-            await conceptos.remove('presentaciones', element.id);
+            await consult.remove('presentaciones', element.id);
         });
-        await conceptos.remove(model, id);
+        await consult.remove(model, id);
         return respuestas.Deleted;
     } catch (error) {
         console.log(`Error en el controlador ${model}, error: ${error}`);
