f5f9a406f2c4b8a9e7e79dc9d4f481f8e7afdff4
Duccem <josemanuel376@hotmail.com>
2019-12-29 16:49:16 -0300

    Hnadling errors on descargos and change the data workflow

--- src/components/descargos/model.ts
+++ src/components/descargos/model.ts
@@ -5,5 +5,14 @@ export interface IDescargo {
     tipo_descargo_id: string | number,
     responsable: string | number,
     autorizador: string | number,
-    detalles:any[]
+    detalles:IDetDescargo[]
+}
+
+export interface IDetDescargo {
+    id?:string | number,
+    enc_descargos_id:string | number,
+    conceptos_id:string | number,
+    depositos_id:string | number,
+    existencia: number,
+    descargo: number
 }
