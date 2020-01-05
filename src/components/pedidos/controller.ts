878c92514575a01a42b18d096a5bd98f2ec78cb6
Duccem <josemanuel376@hotmail.com>
2020-01-05 17:07:53 -0300

    Hnadling errors on pedidos endpoint

--- src/components/pedidos/controller.ts
+++ src/components/pedidos/controller.ts
@@ -1,35 +1,35 @@
 import * as pedidos from '../../helpers/consult';
-import * as links from '../../helpers/links'
-import { Request, response } from 'express';
+import * as links from '../../helpers/links';
+import * as respuestas from '../../errors';
 import { IPedidos, IDetPedidos } from './model';
 
-const model  = "rest_pedidos";
+const model = "rest_pedidos";
 const submodel = "rest_det_pedidos"
 
 /**
  * Get all orders
  * @param query modifier of the consult
  */
-export const get = async (query: any): Promise<any> =>{
+export const get = async (query: any): Promise<any> => {
     try {
-        let data:IPedidos[] = await pedidos.get(model,query);
+        let data: IPedidos[] = await pedidos.get(model, query);
         let totalCount: number = await pedidos.count(model);
         let count = data.length;
         let { limit } = query;
-        if(count > 0){
-            for (let i = 0; i < data.length; i++) {
-                let { id } = data[i];
-                let pres:IDetPedidos[] = await pedidos.getOtherByMe(model, id as string, submodel,{});
-                data[i].detalles = pres;
-            }
-            let link = links.pages(data, model, count, totalCount, limit);
-            let response = Object.assign({ totalCount, count, data }, link);
-            return response;
-        }else{
-            return { message: "No se encontraron registros" }
+        if (count <= 0) return respuestas.Empty;
+
+        for (let i = 0; i < data.length; i++) {
+            let { id } = data[i];
+            let pres: IDetPedidos[] = await pedidos.getOtherByMe(model, id as string, submodel, {});
+            data[i].detalles = pres;
         }
+        let link = links.pages(data, model, count, totalCount, limit);
+        let response = Object.assign({ totalCount, count, data }, link);
+        return { response, code: respuestas.Ok.code };
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -38,24 +38,25 @@ export const get = async (query: any): Promise<any> =>{
  * @param id id of the order
  * @param query modifier of the consult
  */
-export const getOne = async (id:string | number ,query:any): Promise<any> =>{
+export const getOne = async (id: string | number, query: any): Promise<any> => {
     try {
-        if(isNaN(id as number)){
-            return {message:`${id} no es un ID valido`};
-        }
-        let data:IPedidos[] = await pedidos.getOne(model,id,query);
-        let count:number = await pedidos.count(model);
-        if(data[0]){
-            let pres:IDetPedidos[] = await pedidos.getOtherByMe(model, id as string, submodel,{});
-            data[0].detalles = pres;
-            let link = links.records(data,model,count);
-            let response = Object.assign({data},link);
-            return response;
-        }else{
-            return {message:"No se encontro el recurso indicado"};
-        }
+        if (isNaN(id as number)) return respuestas.InvalidID;
+
+        let data: IPedidos = await pedidos.getOne(model, id, query);
+        let count: number = await pedidos.count(model);
+
+        if (!data) return respuestas.Empty;
+
+        let pres: IDetPedidos[] = await pedidos.getOtherByMe(model, id as string, submodel, {});
+        data.detalles = pres;
+        let link = links.records(data, model, count);
+        let response = Object.assign({ data }, link);
+        return { response, code: respuestas.Ok.code };
+
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -63,52 +64,52 @@ export const getOne = async (id:string | number ,query:any): Promise<any> =>{
  * Create a new order
  * @param body data of the new order
  */
-export const create = async (body: any): Promise<any> =>{
-    let {data,data1} = body;
+export const create = async (body: any): Promise<any> => {
+    let { data, data1 } = body;
     let newPedido: IPedidos = data;
     let newDetalles: IDetPedidos[] = data1;
     try {
-        let {insertId} = await pedidos.create(model,newPedido);
-        await pedidos.create(submodel,newDetalles);
+        let { insertId } = await pedidos.create(model, newPedido);
+        await pedidos.create(submodel, newDetalles);
         newPedido.detalles = newDetalles;
-        if(insertId){
-            for (let index = 0; index < newPedido.detalles.length; index++) {
-                let detalle = newPedido.detalles[index];
-                let movDep:any[] = await pedidos.get("movmiento_depositos",{conceptos_id:detalle.conceptos_id});
-                let {affectedRows} = await pedidos.update("movimiento_depositos",movDep[0].id,movDep[0]);
-            }
-            let link = links.created(model,insertId);
-            let response = Object.assign({message:"Registro insertado en la base de datos"},{link:link});
-            return {response,code:201};
+
+        for (let index = 0; index < newPedido.detalles.length; index++) {
+            let detalle = newPedido.detalles[index];
+            let movDep: any[] = await pedidos.get("movmiento_depositos", { conceptos_id: detalle.conceptos_id });
+            await pedidos.update("movimiento_depositos", movDep[0].id, movDep[0]);
         }
-        return {response:"Error al crear entidad"};
+        let link = links.created(model, insertId);
+        let response = Object.assign({ message: respuestas.Created.message }, { link: link });
+        return { response, code: respuestas.Created.code };
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
-
 
 /**
  * Delete a order
  * @param params params request object
  */
-export const remove = async (params: any):Promise<any> => {
+export const remove = async (params: any): Promise<any> => {
     let { id } = params;
     try {
-        if(isNaN(id)) return {message:`${id} is not a valid ID`,code:400};
+        if (isNaN(id)) return respuestas.InvalidID;
 
-        const data:IPedidos = await pedidos.getOne(model,id,{fields:'id'});
-        if(!data) return {message:"Recurso no encontrado",code:404};
+        const data: IPedidos = await pedidos.getOne(model, id, { fields: 'id' });
+        if (!data) return respuestas.ElementNotFound;
 
-        const data1:IDetPedidos[] = await pedidos.get(submodel,{fields:'id'});
-        data1.forEach(async (element:any) => {
-            await pedidos.remove(submodel,element.id);
+        const data1: IDetPedidos[] = await pedidos.get(submodel, { fields: 'id' });
+        data1.forEach(async (element: any) => {
+            await pedidos.remove(submodel, element.id);
         });
-        await pedidos.remove(model,id);
-        const response = {message:"Registro eliminado en la base de datos",code:201}
-        return response;
+        await pedidos.remove(model, id);
+        return respuestas.Deleted;
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -120,18 +121,19 @@ export const remove = async (params: any):Promise<any> => {
 export const addDetail = async (params: any, body: any): Promise<any> => {
     let { data } = body;
     let { id } = params;
-    if(isNaN(id)) return {message:`${id} No es un ID valido`,code:400};
+    if (isNaN(id)) return respuestas.InvalidID;
     try {
-        const pedido = await pedidos.getOne(model,id,{fields:'id'});
-        if(!pedido) return {message:"Recurso no encontrado",code:404};
+        const pedido = await pedidos.getOne(model, id, { fields: 'id' });
+        if (!pedido) return respuestas.ElementNotFound;
         const newDetail: IDetPedidos = data;
-        const { insertId } = await pedidos.create(model,newDetail);
-        if(!insertId) return {message:"Internal server error",code:500};
-        const link = links.created(model,insertId);
-        const response = {message:"Registro insertado en la base de datos",link:link,code:201}
-        return response;
+        const { insertId } = await pedidos.create(model, newDetail);
+        const link = links.created(model, insertId);
+        const response = { message: respuestas.Created.message, link: link }
+        return { response, code: respuestas.Created.code };
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -140,26 +142,26 @@ export const addDetail = async (params: any, body: any): Promise<any> => {
  * @param params params request object
  * @param body data of the detail
  */
-export const updateDetail = async(params: any, body: any): Promise<any> => {
+export const updateDetail = async (params: any, body: any): Promise<any> => {
     let { data } = body;
-    let { id,id1 } = params;
-    if(isNaN(id) || isNaN(id1)) return {message:`${id} o ${id1} No son un ID valido`,code:400};
+    let { id, id1 } = params;
+    if (isNaN(id) || isNaN(id1)) return respuestas.InvalidID;
     try {
-        const pedido = await pedidos.getOne(model,id,{fields:'id'});
-        if(!pedido) return {message:"Recurso no encontrado",code:404};
+        const pedido = await pedidos.getOne(model, id, { fields: 'id' });
+        if (!pedido) return respuestas.ElementNotFound;
 
-        const detalle = await pedidos.getOne(submodel,id1,{fields:'id'});
-        if(!detalle) return {message:"Recurso no encontrado",code:404};
+        const detalle = await pedidos.getOne(submodel, id1, { fields: 'id' });
+        if (!detalle) return respuestas.ElementNotFound;
 
         const newDetail: IDetPedidos = data;
-        const { affectedRows } = await pedidos.update(submodel,id1,newDetail);
-
-        if(!affectedRows) return {message:"Internal server error",code:500};
-        const link = links.created(model,id);
-        const response = {message:"Registro actualizdo en la base de datos",link:link,code:201}
-        return response;
+        const { affectedRows } = await pedidos.update(submodel, id1, newDetail);
+        const link = links.created(model, id);
+        const response = { message: respuestas.Update.message, link: link}
+        return { response, code: respuestas.Update.code };
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -168,17 +170,19 @@ export const updateDetail = async(params: any, body: any): Promise<any> => {
  * Remove a detail from the order
  * @param params params request object
  */
-export const deleteDetail = async (params: any):Promise<any> => {
-    let { id,id1 } = params;
-    if(isNaN(id) || isNaN(id1)) return {message:`${id} o ${id1} No son un ID valido`,code:400};
+export const deleteDetail = async (params: any): Promise<any> => {
+    let { id, id1 } = params;
+    if (isNaN(id) || isNaN(id1)) return respuestas.InvalidID;
     try {
-        const pedido = await pedidos.getOne(model,id,{fields:'id'});
-        if(!pedido) return {message:"Recurso no encontrado",code:404};
-        
-        await pedidos.remove(submodel,id1);
-        const response = {message:"Registro eliminado en la base de datos",code:201}
-        return response;
+        const pedido = await pedidos.getOne(model, id, { fields: 'id' });
+        if (!pedido) return respuestas.ElementNotFound;
+
+        await pedidos.remove(submodel, id1);
+
+        return respuestas.Deleted;
     } catch (error) {
-        throw new Error(`Error en el controlador ${model}, error: ${error}`);
+        if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
