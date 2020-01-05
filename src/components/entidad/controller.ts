d0150528b890a9de4378842d1608a8f561a0998c
Duccem <josemanuel376@hotmail.com>
2020-01-05 11:08:50 -0300

    Hnafling errors on entidad endpoint

--- src/components/entidad/controller.ts
+++ src/components/entidad/controller.ts
@@ -1,6 +1,6 @@
-import * as entidad from '../../helpers/consult';
-import * as links from '../../helpers/links'
-import { Request } from 'express';
+import * as consult from '../../helpers/consult';
+import * as links from '../../helpers/links';
+import * as respuestas from '../../errors';
 import { IEntidad } from './model';
 
 const model = "entidad";
@@ -9,21 +9,23 @@ const model = "entidad";
  * Get all entities
  * @param query modifier of the consult
  */
-export const get = async (query: any): Promise<any> =>{
+export const get = async (query: any): Promise<any> => {
     try {
-        let data:IEntidad[] = await entidad.get(model,query);
-        let totalCount: number = await entidad.count(model);
+        let data: IEntidad[] = await consult.get(model, query);
+        let totalCount: number = await consult.count(model);
         let count = data.length;
         let { limit } = query;
-        if(count > 0){
-            let link = links.pages(data, 'entidad', count, totalCount, limit);
-            let response = Object.assign({ totalCount, count, data }, link);
-            return response;
-        }else{
-            return { message: "No se encontraron registros" }
-        }
+
+        if (count <= 0) return respuestas.Empty;
+        
+        let link = links.pages(data, model, count, totalCount, limit);
+        let response = Object.assign({ totalCount, count, data }, link);
+        return { response, code: respuestas.Ok.code };
+
     } catch (error) {
-        throw new Error(`Error al consultar la base de datos, error: ${error}`);
+        if(error.message ==='BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -32,23 +34,22 @@ export const get = async (query: any): Promise<any> =>{
  * @param id id of the entity
  * @param query modifier of the consult
  */
-export const getOne = async (id:string | number ,query:any): Promise<any> =>{
+export const getOne = async (id: string | number, query: any): Promise<any> => {
     try {
-        if(isNaN(id as number)){
-            return {message:`${id} no es un ID valido`};
-        }
-        let data:IEntidad[] = await entidad.getOne(model,id,query);
-        let count:number = await entidad.count(model);
-        if(data[0]){
-            let link = links.records(data,model,count);
-            
-            let response = Object.assign({data},link);
-            return response;
-        }else{
-            return {message:"No se encontro el recurso indicado"};
-        }
+        if (isNaN(id as number)) return respuestas.InvalidID;
+        
+        let data: IEntidad = await consult.getOne(model, id, query);
+        let count: number = await consult.count(model);
+
+        if (!data) return respuestas.ElementNotFound;
+        
+        let link = links.records(data, model, count);
+        let response = Object.assign({ data }, link);
+        return {response,code:respuestas.Ok.code};
     } catch (error) {
-        throw new Error(`Error al consultar la base de datos, error: ${error}`);
+        if(error.message ==='BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -56,16 +57,18 @@ export const getOne = async (id:string | number ,query:any): Promise<any> =>{
  * Create a new entity
  * @param body data of the new entity
  */
-export const create = async (body: any): Promise<any> =>{
-    let {data} = body;
+export const create = async (body: any): Promise<any> => {
+    let { data } = body;
     let newArea: IEntidad = data;
     try {
-        let {insertId} = await entidad.create(model,newArea);
-        let link = links.created('entidad',insertId);
-        let response = Object.assign({message:"Registro insertado en la base de datos"},{link:link});
-        return {response,code:201};
+        let { insertId } = await consult.create(model, newArea);
+        let link = links.created(model, insertId);
+        let response = Object.assign({ message: respuestas.Created.message }, { link: link });
+        return { response, code: respuestas.Created.code };
     } catch (error) {
-        throw new Error(`Error al consultar la base de datos, error: ${error}`);
+        if(error.message ==='BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
@@ -74,27 +77,33 @@ export const create = async (body: any): Promise<any> =>{
  * @param params paramas request object
  * @param body data of the entity
  */
-export const update = async (params: any, body:any): Promise<any>=>{
-    const {id} = params;
-    let {data} = body;
-    let newArea:IEntidad = data;
+export const update = async (params: any, body: any): Promise<any> => {
+    const { id } = params;
+    let { data } = body;
+    let newArea: IEntidad = data;
 
     try {
-        let {affectedRows}  = await entidad.update(model,id,newArea);
-        let link = links.created('entidad',id);
-        let response = Object.assign({message:"Registro actualizado en la base de datos",affectedRows},{link:link});
-        return {response,code:201};
+        if(isNaN(id)) return respuestas.InvalidID;
+
+        let { affectedRows } = await consult.update(model, id, newArea);
+        let link = links.created(model, id);
+        let response = Object.assign({ message: respuestas.Update.message, affectedRows }, { link: link });
+        return { response, code: respuestas.Update.code };
     } catch (error) {
-        throw new Error(`Error al consultar la base de datos, error: ${error}`);
+        if(error.message ==='BD_SYNTAX_ERROR') return respuestas.BadRequest;
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
 
-export const remove = async (params: any):Promise<any> => {
-    let {id} = params;
+export const remove = async (params: any): Promise<any> => {
+    let { id } = params;
     try {
-        await entidad.remove(model,id);
-        return {response:{message:"Registro eliminado de la base de datos"},code:200};   
+        if(isNaN(id)) return respuestas.InvalidID;
+        await consult.remove(model, id);
+        return respuestas.Deleted;
     } catch (error) {
-        throw new Error(`Error al consultar la base de datos, error: ${error}`);
+        console.log(`Error al consultar la base de datos, error: ${error}`);
+        return respuestas.InternalServerError;
     }
 }
