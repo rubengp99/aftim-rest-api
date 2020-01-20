2564a6542efd26090f2659fb96ec6adf016cbe6b
Duccem <josemanuel376@hotmail.com>
2020-01-20 14:17:03 -0300

    some changes

--- src/components/usuario/controller.ts
+++ src/components/usuario/controller.ts
@@ -11,15 +11,16 @@ const model = "usuario";
  * Validate the user data
  * @param body user data
  */
-export const login = async (body:any):Promise<any> =>{
-    let {usuario,password} = body.data;
-    if(usuario === '' || password === '') return respuestas.Unauthorized;
+export const login = async (body: any): Promise<any> => {
+    let { usuario, password } = body.data;
+    if (usuario === '' || password === '') return respuestas.Unauthorized;
     try {
-        let user:IUsuario = await consult.getUser(usuario);
-        if(!user || !encript.validar(password,user.password)) return respuestas.Unauthorized;
-        const token:string = jwt.sign({_id:user.login}, tokenKey || "2423503",{ expiresIn: 60 * 60 * 24});
-        const response = {data:user};
-        return {response,token,code:respuestas.Ok.code};
+        let user: IUsuario = await consult.getUser(usuario);
+        if (!user) return respuestas.Unauthorized;
+        if (!encript.validar(password, user.password)) return respuestas.Unauthorized;
+        const token: string = jwt.sign({ _id: user.login }, tokenKey || "2423503", { expiresIn: 60 * 60 * 24 });
+        const response = { data: user };
+        return { response, token, code: respuestas.Ok.code };
     } catch (error) {
         if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
         console.log(`Error en el controlador ${model}, error: ${error}`);
@@ -34,13 +35,13 @@ export const login = async (body:any):Promise<any> =>{
  */
 export const signUp = async (body: any): Promise<any> => {
     const { data } = body;
-    const newUser:IUsuario = data; 
+    const newUser: IUsuario = data;
     newUser.password = await encript.encriptar(newUser.password);
     try {
-        let {insertId} = await consult.create(model,newUser);
-        const token:string = jwt.sign({_id:newUser.login},tokenKey || "2423503",{ expiresIn: 60 * 60 * 24});
-        const response = {data:newUser}
-        return {token,response,code:respuestas.Created.code};
+        let { insertId } = await consult.create(model, newUser);
+        const token: string = jwt.sign({ _id: newUser.login }, tokenKey || "2423503", { expiresIn: 60 * 60 * 24 });
+        const response = { data: newUser }
+        return { token, response, code: respuestas.Created.code };
     } catch (error) {
         if (error.message === 'BD_SYNTAX_ERROR') return respuestas.BadRequest;
         console.log(`Error en el controlador ${model}, error: ${error}`);
@@ -52,25 +53,25 @@ export const signUp = async (body: any): Promise<any> => {
  * Validate a token
  * @param headers headers request object
  */
-export const validarToken = async (headers: any): Promise<any> =>{
-    const { user_token }  = headers;
+export const validarToken = async (headers: any): Promise<any> => {
+    const { user_token } = headers;
     try {
-        if(!user_token) return respuestas.Unauthorized;
-        let payload:any = jwt.verify(user_token,tokenKey || "2423503");
-        let user:IUsuario = await consult.getUser(payload._id);
+        if (!user_token) return respuestas.Unauthorized;
+        let payload: any = jwt.verify(user_token, tokenKey || "2423503");
+        let user: IUsuario = await consult.getUser(payload._id);
 
-        if(!user) return respuestas.Unauthorized;
-        const response = { data:user };
-        return {response,code:respuestas.Ok.code};
+        if (!user) return respuestas.Unauthorized;
+        const response = { data: user };
+        return { response, code: respuestas.Ok.code };
     } catch (error) {
-        if(error.name=="TokenExpiredError") return respuestas.Forbidden;
-        if(error.message=="BD_SYNTAX_ERROR") return respuestas.BadRequest;
+        if (error.name == "TokenExpiredError") return respuestas.Forbidden;
+        if (error.message == "BD_SYNTAX_ERROR") return respuestas.BadRequest;
         console.log(`Error desconocido al iniciar sesion, Error: ${error}`);
         return respuestas.InternalServerError;
     }
 }
 
-export const getPedidosByUser = async (id: string | number, query: any): Promise<any> =>{
+export const getPedidosByUser = async (id: string | number, query: any): Promise<any> => {
     try {
         if (isNaN(id as number)) return respuestas.InvalidID;
 
