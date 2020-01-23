a602e7ae9cf00148622806b8ef20e1ad8c955953
Duccem <josemanuel376@hotmail.com>
2020-01-23 15:28:54 -0300

    Forget fix route on concepts

--- src/components/conceptos/route.ts
+++ src/components/conceptos/route.ts
@@ -88,7 +88,7 @@ router.post('/',validar, async (req:Request, res:Response):Promise<Response> => 
 });
 router.post('/:id',validar, async (req:Request, res:Response):Promise<Response> => {
     try {
-        let {message,response,code} = await controller.update(req.params,req.body);
+        let {message,response,code} = await controller.update(req.params,req.body,req.file);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
