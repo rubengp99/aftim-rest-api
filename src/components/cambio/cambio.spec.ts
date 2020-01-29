b5c5820dde0f566690607bd6a35e84525fd930da
Duccem <josemanuel376@hotmail.com>
2020-01-29 16:09:02 -0300

    some chages

--- src/components/cambio/cambio.spec.ts
+++ src/components/cambio/cambio.spec.ts
@@ -1,12 +1,18 @@
-import { get } from './controller';
+import { get, getOne } from './controller';
 import { connect,disconnect } from '../../dbs';
+
 connect();
-describe('Get',()=>{
+describe('Cambio controller',()=>{
     beforeAll(()=>{
         connect();
     })
-    test('Response', async ()=>{
+    test('Get', async ()=>{
         const data = await get({});
+        expect(data.code).toBe(200);
+        expect(data.response).toBeDefined();
+    });
+    test('Get One', async () =>{
+        const data = await getOne(1,{});
         expect(data.code).toBe(200);
         expect(data.response).toBeDefined();
     });
