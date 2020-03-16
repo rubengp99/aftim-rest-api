420b0c2653de9d5cc017b25f988273b19640ce9e
Duccem <josemanuel376@hotmail.com>
2020-03-16 14:14:55 -0300

    more fixes

--- src/components/pedidos/controller.ts
+++ src/components/pedidos/controller.ts
@@ -133,9 +133,9 @@ export const remove = async (params: any): Promise<any> => {
 
         const data1: IDetPedidos[] = await consult.getOtherByMe(model, id, submodel, {});
         for (let index = 0; index < data1.length; index++) {
-            let movDep: any[] = await consult.get("movimiento_deposito", { conceptos_id: data1[index].conceptos_id });
+            let movDep: any[] = await consult.get("adm_movimiento_deposito", { conceptos_id: data1[index].adm_conceptos_id });
             movDep[0].existencia = parseFloat(movDep[0].existencia) + parseFloat(data1[index].cantidad as unknown as string);
-            await consult.update("movimiento_deposito", movDep[0].id, movDep[0]);
+            await consult.update("adm_movimiento_deposito", movDep[0].id, movDep[0]);
             await consult.remove(submodel, data1[index].id as number);
 
         }
@@ -166,7 +166,7 @@ export const addDetail = async (params: any, body: any): Promise<any> => {
 
         newDetail.id = insertId;
 
-        let movDep: any[] = await consult.get("adm_movimiento_deposito", { conceptos_id: newDetail.conceptos_id });
+        let movDep: any[] = await consult.get("adm_movimiento_deposito", { conceptos_id: newDetail.adm_conceptos_id });
         movDep[0].existencia = movDep[0].existencia - newDetail.cantidad;
         await consult.update("adm_movimiento_deposito", movDep[0].id, movDep[0]);
 
@@ -199,9 +199,9 @@ export const updateDetail = async (params: any, body: any): Promise<any> => {
         const newDetail: IDetPedidos = data;
 
 
-        let movDep: any[] = await consult.get("movimiento_deposito", { conceptos_id: newDetail.conceptos_id });
+        let movDep: any[] = await consult.get("adm_movimiento_deposito", { conceptos_id: newDetail.adm_conceptos_id });
         movDep[0].existencia = parseFloat(movDep[0].existencia) - (newDetail.cantidad - parseFloat(detalle.cantidad));
-        await consult.update("movimiento_deposito", movDep[0].id, movDep[0]);
+        await consult.update("adm_movimiento_deposito", movDep[0].id, movDep[0]);
 
         await consult.update(submodel, id1, newDetail);
 
@@ -227,9 +227,9 @@ export const deleteDetail = async (params: any): Promise<any> => {
         const pedido = await consult.getOne(model, id, { fields: 'id' });
         if (!pedido) return respuestas.ElementNotFound;
         const detalle = await consult.getOne(submodel, id1, {});
-        let movDep: any[] = await consult.get("movimiento_deposito", { conceptos_id: detalle.conceptos_id });
+        let movDep: any[] = await consult.get("adm_movimiento_deposito", { conceptos_id: detalle.adm_conceptos_id });
         movDep[0].existencia = parseFloat(movDep[0].existencia) + parseFloat(detalle.cantidad);
-        await consult.update("movimiento_deposito", movDep[0].id, movDep[0]);
+        await consult.update("adm_movimiento_deposito", movDep[0].id, movDep[0]);
 
         await consult.remove(submodel, id1);
 
