0193d0d0d30c7d33ba62a2a4ee3bc98c26c8edad
Duccem <josemanuel376@hotmail.com>
2020-04-03 12:46:27 -0300

    pages

---
+++ pages/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
+const history = require('connect-history-api-fallback');
+
+
+const app = express();
+
+app.set('port', process.argv[2] || process.env.PAGES_PORT || 85);
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use('/hoyprovoca/',history())
+app.use('/',express.static(path.resolve('public/pages/')));
+
+module.exports = app;
