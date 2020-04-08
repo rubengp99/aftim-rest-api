c8735efb4fc14a74fdd5f514260d46f83e94ac4b
Duccem <josemanuel376@hotmail.com>
2020-04-08 15:56:28 -0400

    added the model mov bank to the images microservice

--- images/media/photo/models.js
+++ images/media/photo/models.js
@@ -5,6 +5,7 @@ module.exports = {
         grupos:"adm_grupos",
         subgrupos:"adm_subgrupos",
         pedidos:"rest_pedidos",
-        det_pedidos:"rest_det_pedidos"
+        det_pedidos:"rest_det_pedidos",
+        movimiento_banco:"adm_movimientos_bancos"
     }
 }
