74c03e9c5ec935793856c39a9fb13a3362104ad1
Duccem <josemanuel376@hotmail.com>
2020-05-20 14:40:33 -0400

    issold route added to concepts

--- server/api/src/components/conceptos/route.ts
+++ server/api/src/components/conceptos/route.ts
@@ -1,146 +1,186 @@
-import * as controller  from './controller';
-import  {validar}  from '../../helpers/aunthentication';
-import { InternalServerError } from '../../errors';
-import  { Router,Request,Response } from 'express';
+import * as controller from "./controller";
+import { validar } from "../../helpers/aunthentication";
+import { InternalServerError } from "../../errors";
+import { Router, Request, Response } from "express";
 const router = Router();
 //obtener todos los conceptos
-router.get('/',validar, async (req:Request, res:Response):Promise<Response> => {
-    try {
-        let { message, response, code } = await controller.get(req.query);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
-router.get('/mostsold',validar, async (req:Request, res:Response): Promise<Response> =>{
-    try {
-        let { message, response, code } = await controller.getMostSold(req.query);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
-router.get('/mostreturned',validar, async (req:Request, res:Response): Promise<Response> =>{
-    try {
-        let { message, response, code } = await controller.getMostReturned(req.query);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
-router.get('/:id/sell',validar, async (req:Request, res:Response): Promise<Response> =>{
-    try {
-        let { message, response, code } = await controller.sellByConcept(req.params,req.query);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
-router.get('/:id/devolutions',validar, async (req:Request, res:Response): Promise<Response> =>{
-    try {
-        let { message, response, code } = await controller.devolutionsByConcept(req.params,req.query);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
-router.get('/:id',validar, async (req:Request, res:Response):Promise<Response> => {
-    let {id} = req.params;
-    try {
-        let { message, response, code } = await controller.getOne(id,req.query);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
-router.get('/:id/depositos',validar, async (req:Request, res:Response):Promise<Response> => {
-    let {id} = req.params;
-    try {
-        let { message, response, code } = await controller.getDepositsByConcept(id,req.query);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
-router.get('/:id/photos',validar, async (req:Request, res:Response):Promise<Response> => {
-    let {id} = req.params;
-    try {
-        let { message, response, code } = await controller.getPhotosByConcept(id,req.query);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
-router.get('/:id/presentaciones',validar, async (req:Request, res:Response):Promise<Response> => {
-    let {id} = req.params;
-    try {
-        let { message, response, code } = await controller.getPresentationsByConcept(id,req.query);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
+router.get(
+	"/",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		try {
+			let { message, response, code } = await controller.get(req.query);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
+router.get(
+	"/mostsold",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		try {
+			let { message, response, code } = await controller.getMostSold(req.query);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
 
-router.post('/',validar, async (req:Request, res:Response):Promise<Response> => {
-    try {
-        let {message,response,code} = await controller.create(req.body,req.file);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
-router.post('/:id',validar, async (req:Request, res:Response):Promise<Response> => {
-    try {
-        let {message,response,code} = await controller.update(req.params,req.body,req.file);
-        return res.status(code).json(message || response);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
+router.get(
+	"/mostreturned",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		try {
+			let { message, response, code } = await controller.getMostReturned(req.query);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
+
+router.get(
+	"/:id/sell",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		try {
+			let { message, response, code } = await controller.sellByConcept(req.params, req.query);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
+router.get(
+	"/:id/devolutions",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		try {
+			let { message, response, code } = await controller.devolutionsByConcept(req.params, req.query);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
+router.get(
+	"/:id/issold",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		let { id } = req.params;
+		try {
+			let { message, response, code } = await controller.isSold(id);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
+router.get(
+	"/:id",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		let { id } = req.params;
+		try {
+			let { message, response, code } = await controller.getOne(id, req.query);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
+router.get(
+	"/:id/depositos",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		let { id } = req.params;
+		try {
+			let { message, response, code } = await controller.getDepositsByConcept(id, req.query);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
+router.get(
+	"/:id/photos",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		let { id } = req.params;
+		try {
+			let { message, response, code } = await controller.getPhotosByConcept(id, req.query);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
+router.get(
+	"/:id/presentaciones",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		let { id } = req.params;
+		try {
+			let { message, response, code } = await controller.getPresentationsByConcept(id, req.query);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
+
+router.post(
+	"/",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		try {
+			let { message, response, code } = await controller.create(req.body, req.file);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
+router.post(
+	"/:id",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		try {
+			let { message, response, code } = await controller.update(req.params, req.body, req.file);
+			return res.status(code).json(message ? { message } : response);
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
 //eliminar una grupo
-router.delete('/:id',validar, async (req:Request, res:Response):Promise<Response> => {
-    try {
-        let {message,code} = await controller.remove(req.params);
-        return res.status(code).json(message);
-    } catch (error) {
-        console.log(error);
-        return res
-                .status(InternalServerError.code)
-                .json({message:InternalServerError.message});
-    }
-});
+router.delete(
+	"/:id",
+	validar,
+	async (req: Request, res: Response): Promise<Response> => {
+		try {
+			let { message, code } = await controller.remove(req.params);
+			return res.status(code).json({ message });
+		} catch (error) {
+			console.log(error);
+			return res.status(InternalServerError.code).json({ message: InternalServerError.message });
+		}
+	}
+);
 
 export default router;
