a8ca3f5d41ca99bdbc54cc0ab4621b4bb3d8e2fb
tabascospicy <rafaeltabasca@gmail.com>
2020-06-09 11:56:30 -0400

    ready tipos scheme

--- server/api/src/components/tipos/scheme.ts
+++ server/api/src/components/tipos/scheme.ts
@@ -1,6 +1,49 @@
 import * as mongoose from "mongoose";
-const TipoScheme = new mongoose.Schema({
-
+export const ITipoConceptosScheme = new mongoose.Schema({
+    id: {
+        type: String || Number,
+        required: false,
+    },
+    nombre: {
+        type: String,
+        required: true,
+        default: null
+    }
 })
-const model = mongoose.model("Tipo", TipoScheme, "Tipo");
-export default model;
+export const ITipoCuentaScheme = new mongoose.Schema({
+    id: {
+        type: String || Number,
+        required: false,
+    },
+    nombre: {
+        type: String,
+        required: true,
+        default: null
+    }
+})
+export const ITipoEstatusEnsambladoScheme = new mongoose.Schema({
+    id: {
+        type: String || Number,
+        required: false,
+    },
+    nombre: {
+        type: String,
+        required: true,
+        default: null
+    }
+})
+export const ITipoMovimientoScheme = new mongoose.Schema({
+    id: {
+        type: String || Number,
+        required: false,
+    },
+    nombre: {
+        type: String,
+        required: true,
+        default: null
+    }
+})
+export const TipoConceptosModel = mongoose.model("Tipo_conceptos", ITipoConceptosScheme, "Tipo_conceptos");
+export const TipoCuentaModel = mongoose.model("Tipo_cuenta", ITipoCuentaScheme, "Tipo_cuenta");
+export const TipoEstatusEnsambladoModel = mongoose.model("Tipo_Estatus_Ensamblado", ITipoEstatusEnsambladoScheme, "Tipo_Estatus_Ensamblado");
+export const TipoMovimientoModel = mongoose.model("Tipo_Movimiento", ITipoMovimientoScheme, "Tipo_Movimiento");
