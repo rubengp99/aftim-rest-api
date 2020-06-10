e73eb7582ff880ac647d487bd0e87169d7debbb0
Ruben D. Garcia <rdgp99@gmail.com>
2020-06-09 22:11:50 -0400

    added mongodb connection abstraction

--- server/api/src/keys.ts
+++ server/api/src/keys.ts
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
+export const MONGO_URI = process.env.MONGO_URI
+export const MONGO_DB = process.env.MONGO_DB
 export const tokenKey = process.env.TOKEN_KEY;
 export const dataURL = process.env.DATA_URL;
 export const authURL = process.env.AUTH_URL;
