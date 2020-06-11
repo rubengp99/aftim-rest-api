f24d0946bd436b5e9c8c115782a42dc6b9e2cc5f
Ruben D. Garcia <rdgp99@gmail.com>
2020-06-11 00:12:23 -0400

    added docs for mongo helpers

--- server/api/src/helpers/mongoFuncs.ts
+++ server/api/src/helpers/mongoFuncs.ts
@@ -2,7 +2,17 @@ import mongoose from "mongoose";
 import { Filters, HasValue } from "./query.model"
 import { newMultiTenantConnection } from "../mongoConn"
 
+/**
+ * 
+ * @param tenantId is the identifier of the company which is requesting data
+ * @param colName is the name of the collection data
+ * @returns promise function
+ */
 export const createInsertFunc = async function(tenantId: string, colName: string):Promise<any> {
+    /**
+     * @param schema is the document scheme for mongo usage
+     * @param object is the document object representation to insert
+     */
     return async function (schema: mongoose.Schema, object: Object):Promise<any>{
         try {
             let db = await newMultiTenantConnection(tenantId);
@@ -25,7 +35,17 @@ export const createInsertFunc = async function(tenantId: string, colName: string
     }
 }
 
+/**
+ * 
+ * @param tenantId is the identifier of the company which is requesting data
+ * @param colName is the name of the collection data
+ * @returns promise function
+ */
 export const createRetrieveFunc = async function(tenantId: string, colName: string):Promise<any>{
+    /**
+     * @param schema is the document scheme for mongo usage
+     * @param query is a interface with the mongo query options
+     */
     return async function (schema: mongoose.Schema, query: Filters):Promise<any>{
         try {
             let limit = HasValue(query.limit) ? query.limit as number : 0;
@@ -52,7 +72,17 @@ export const createRetrieveFunc = async function(tenantId: string, colName: stri
     }
 }
 
+/**
+ * 
+ * @param tenantId is the identifier of the company which is requesting data
+ * @param colName is the name of the collection data
+ * @returns promise function
+ */
 export const createRetrieveOneFunc = async function(tenantId: string, colName: string):Promise<any>{
+    /**
+     * @param schema is the document scheme for mongo usage
+     * @param id is the unique identifier of the document
+     */
     return async function (schema: mongoose.Schema, id: string):Promise<any>{
         try {
             let db = await newMultiTenantConnection(tenantId);
@@ -75,7 +105,17 @@ export const createRetrieveOneFunc = async function(tenantId: string, colName: s
     }
 }
 
+/**
+ * 
+ * @param tenantId is the identifier of the company which is requesting data
+ * @param colName is the name of the collection data
+ * @returns promise function
+ */
 export const createDeleteOneFunc = async function(tenantId: string, colName: string):Promise<any>{
+    /**
+     * @param schema is the document scheme for mongo usage
+     * @param opts is a parameter which is need to be meet by the deleted document
+     */
     return async function (schema: mongoose.Schema, opts: object):Promise<any>{
         try {
             let db = await newMultiTenantConnection(tenantId);
@@ -99,6 +139,11 @@ export const createDeleteOneFunc = async function(tenantId: string, colName: str
 }
 
 export const createUpdateFunc = async function(tenantId: string, colName: string):Promise<any>{
+    /**
+     * @param schema is the document scheme for mongo usage
+     * @param id is the unique identifier of the document
+     * @param data is the new information for the object to be updated
+     */
     return async function (schema: mongoose.Schema, id: string, data: object):Promise<any>{
         try {
             let db = await newMultiTenantConnection(tenantId);
