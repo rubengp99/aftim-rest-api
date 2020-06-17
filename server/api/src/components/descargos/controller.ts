412d92ddf1f7d5c56357c8fa1b0e1f82c3792bf5
Ruben D. Garcia <rdgp99@gmail.com>
2020-06-16 23:20:05 -0400

    added multi tenant paradigm to descargos ENDPOINT

--- server/api/src/components/descargos/controller.ts
+++ server/api/src/components/descargos/controller.ts
@@ -10,10 +10,10 @@ const submodel = "adm_enc_descargos";
  * Get all descargos
  * @param query modifier of the consult 
  */
-export const get = async (query: any): Promise<any> => {
+export const get = async (query: any, tenantId: string): Promise<any> => {
     try {
-        let data: IDescargo[] = await consult.get(model, query);
-        let totalCount: number = await consult.count(model);
+        let data: IDescargo[] = await consult.get(tenantId, model, query);
+        let totalCount: number = await consult.count(tenantId, model);
         let count = data.length;
         let { limit } = query;
 
@@ -21,7 +21,7 @@ export const get = async (query: any): Promise<any> => {
 
         for (let i = 0; i < data.length; i++) {
             let { id } = data[i];
-            let pres: IDetDescargo[] = await consult.getOtherByMe(model, id as string, submodel);
+            let pres: IDetDescargo[] = await consult.getOtherByMe(tenantId, model, id as string, submodel);
             data[i].detalles = pres;
         }
 
@@ -42,16 +42,16 @@ export const get = async (query: any): Promise<any> => {
  * @param id id of the descargo
  * @param query modifier of the descargo
  */
-export const getOne = async (id: string | number, query: any): Promise<any> => {
+export const getOne = async (id: string | number, query: any, tenantId: string): Promise<any> => {
     try {
         if (isNaN(id as number)) return respuestas.InvalidID;
 
-        let data: IDescargo = await consult.getOne(model, id, query);
-        let count: number = await consult.count(model);
+        let data: IDescargo = await consult.getOne(tenantId, model, id, query);
+        let count: number = await consult.count(tenantId, model);
 
         if (!data) return respuestas.ElementNotFound;
 
-        let pres: IDetDescargo[] = await consult.getOtherByMe(model, id as string, submodel);
+        let pres: IDetDescargo[] = await consult.getOtherByMe(tenantId, model, id as string, submodel);
         data.detalles = pres;
         let link = links.records(data, 'descargos', count);
         let response = Object.assign({ data }, link);
@@ -69,18 +69,18 @@ export const getOne = async (id: string | number, query: any): Promise<any> => {
  * Create a descargo
  * @param body data of the new descargo
  */
-export const create = async (body: any): Promise<any> => {
+export const create = async (body: any, tenantId: string): Promise<any> => {
     let { data,data1 } = body;
     let newCargo: IDescargo = data;
     let detalles: IDetDescargo[] = data1;
     try {
-        let { insertId } = await consult.create(model, newCargo);
-        await consult.create(submodel,detalles);
+        let { insertId } = await consult.create(tenantId, model, newCargo);
+        await consult.create(tenantId, submodel,detalles);
         
         for (let index = 0; index < detalles.length; index++) {
             let detalle = detalles[index];
-            let movDep: any[] = await consult.get("movmiento_depositos", { depositos_id: detalle.depositos_id, conceptos_id: detalle.conceptos_id });
-            await consult.update("movimiento_depositos", movDep[0].id, movDep[0]);
+            let movDep: any[] = await consult.get(tenantId, "movmiento_depositos", { depositos_id: detalle.depositos_id, conceptos_id: detalle.conceptos_id });
+            await consult.update(tenantId, "movimiento_depositos", movDep[0].id, movDep[0]);
         }
         
         let link = links.created('descargos', insertId);
