1c388edb31154d8a2984016dc3b5d5c6540ef325
Ruben D. Garcia <rdgp99@gmail.com>
2020-06-17 01:13:49 -0400

    fixed errors on multi tenant paradigm

--- server/api/src/components/ciudad/route.ts
+++ server/api/src/components/ciudad/route.ts
@@ -7,7 +7,8 @@ const router = Router();
 //obtener todas las ciudades
 router.get('/', validar, async (req: Request, res: Response): Promise<Response> => {
     try {
-        let { message, response, code } = await controller.get(req.query);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { message, response, code } = await controller.get(req.query, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -19,7 +20,8 @@ router.get('/', validar, async (req: Request, res: Response): Promise<Response> 
 router.get('/:id', validar, async (req: Request, res: Response): Promise<Response> => {
     let { id } = req.params;
     try {
-        let { message, response, code } = await controller.getOne(id, req.query);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { message, response, code } = await controller.getOne(id, req.query, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -30,7 +32,8 @@ router.get('/:id', validar, async (req: Request, res: Response): Promise<Respons
 //crear un registro de ciudad
 router.post('/', validar, async (req: Request, res: Response): Promise<Response> => {
     try {
-        let { message, response, code } = await controller.create(req.body);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { message, response, code } = await controller.create(req.body, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
