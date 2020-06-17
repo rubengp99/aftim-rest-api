67f1f365e5b07566571969e647880e171849e1fc
Ruben D. Garcia <rdgp99@gmail.com>
2020-06-17 01:08:28 -0400

    added multi tenant paradigm to usuario ENDPOINT

--- server/api/src/components/usuario/route.ts
+++ server/api/src/components/usuario/route.ts
@@ -8,7 +8,8 @@ const router = Router();
 router.get('/',validar, async(req:Request, res:Response): Promise<Response> =>{
     let {query} = req;
     try {
-        let { code, response, message } = await controller.get(query);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { code, response, message } = await controller.get(query, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -20,7 +21,8 @@ router.get('/',validar, async(req:Request, res:Response): Promise<Response> =>{
 router.get('/:id',validar, async (req:Request, res:Response):Promise<Response> => {
     let {id} = req.params;
     try {
-        let {message,response,code} = await controller.getOne(id,req.query);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let {message,response,code} = await controller.getOne(id,req.query, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -33,7 +35,8 @@ router.get('/:id/pedidos',validar, async(req:Request, res:Response): Promise<Res
     let {id} = req.params;
     let {query} = req;
     try {
-        let { code, response, message } = await controller.getPedidosByUser(id,query);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { code, response, message } = await controller.getPedidosByUser(id,query, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -44,7 +47,8 @@ router.get('/:id/pedidos',validar, async(req:Request, res:Response): Promise<Res
 //actualizar un usuario
 router.post('/:id',validar, async (req:Request, res:Response):Promise<Response> => {
     try {
-        let {message,response,code} = await controller.update(req.params,req.body);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let {message,response,code} = await controller.update(req.params,req.body, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -55,7 +59,8 @@ router.post('/:id',validar, async (req:Request, res:Response):Promise<Response> 
 //eliminar un usuario
 router.delete('/:id',validar, async (req:Request, res:Response):Promise<Response> => {
     try {
-        let {message,response,code} = await controller.remove(req.params);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let {message,response,code} = await controller.remove(req.params, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
