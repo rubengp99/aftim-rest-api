126f87a9cb021e75405200162bb766a9dd5e559f
Ruben D. Garcia <rdgp99@gmail.com>
2020-06-16 23:18:31 -0400

    added multi tenant paradigm to depositos ENDPOINT

--- server/api/src/components/depositos/route.ts
+++ server/api/src/components/depositos/route.ts
@@ -7,7 +7,8 @@ const router = Router();
 //obtener todos los depositos
 router.get('/', validar, async (req: Request, res: Response): Promise<Response> => {
     try {
-        let { message, response, code } = await controller.get(req.query);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { message, response, code } = await controller.get(req.query, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -19,7 +20,8 @@ router.get('/', validar, async (req: Request, res: Response): Promise<Response> 
 router.get('/:id', validar, async (req: Request, res: Response): Promise<Response> => {
     let { id } = req.params;
     try {
-        let { message, response, code } = await controller.getOne(id, req.query);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { message, response, code } = await controller.getOne(id, req.query, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -32,7 +34,8 @@ router.get('/:id/conceptos/', validar, async (req: Request, res: Response): Prom
     let { id } = req.params;
     let { query } = req;
     try {
-        let { message, response, code } = await controller.getConceptosBydeposito(id, query);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { message, response, code } = await controller.getConceptosBydeposito(id, query, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -43,7 +46,8 @@ router.get('/:id/conceptos/', validar, async (req: Request, res: Response): Prom
 //crear un deposito
 router.post('/', validar, async (req: Request, res: Response): Promise<Response> => {
     try {
-        let { message, response, code } = await controller.create(req.body);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { message, response, code } = await controller.create(req.body, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -54,7 +58,8 @@ router.post('/', validar, async (req: Request, res: Response): Promise<Response>
 //actualizar un deposito
 router.post('/:id', validar, async (req: Request, res: Response): Promise<Response> => {
     try {
-        let { message, response, code } = await controller.update(req.params, req.body);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { message, response, code } = await controller.update(req.params, req.body, tenantId);
         return res.status(code).json(message || response);
     } catch (error) {
         console.log(error);
@@ -64,7 +69,8 @@ router.post('/:id', validar, async (req: Request, res: Response): Promise<Respon
 //eliminar un deposito
 router.delete('/:id', validar, async (req: Request, res: Response): Promise<Response> => {
     try {
-        let { message, code } = await controller.remove(req.params);
+        let tenantId: string = req.headers['tenantId'] as string;
+        let { message, code } = await controller.remove(req.params, tenantId);
         return res.status(code).json(message);
     } catch (error) {
         console.log(error);
