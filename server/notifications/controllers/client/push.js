a37e707c0a9220e1c6e19d4f76dba7bde2c654ca
SpicyT <rafaeltabasca@gmail.com>
2020-06-22 17:09:24 -0400

    fixing issues

--- server/notifications/controllers/client/push.js
+++ server/notifications/controllers/client/push.js
@@ -4,7 +4,7 @@ const router = Router();
 const { web_push, mail, DATA_URL } = require("../../keys");
 const webPush = require("web-push");
 
-const {createAxios} = require("./../../axios");
+const { createAxios } = require("./../../axios");
 
 webPush.setVapidDetails(
     `mailto:${mail.MAIL}`,
@@ -17,20 +17,23 @@ const tenantId = "almendras";
 
 router.post("/subscribe", async (req, res) => {
     try {
-       const parsed_data= JSON.parse(req.body.data);
-        let {subscription_data,usuario_id}  = parsed_data;
-        const connection = createAxios(baseURL,tenantId);
-        const {auth,p256dh} = subscription_data.keys;
-        const {endpoint,expirationTime} = subscription_data;
+        let { data } = req.body;
+        if (typeof data === "undefined") return res.status(400).json({ message: "bad request" });
+        let { subscription_data, usuario_id } = parsed_data;
+        const connection = createAxios(baseURL, tenantId);
+        const { auth, p256dh } = subscription_data.keys;
+        const { endpoint, expirationTime } = subscription_data;
         const toSave = {
-            auth:auth,
-            p256dh:p256dh,
-            endpoint:endpoint,
-            expiration_time:expirationTime,
-            usuario_id:usuario_id
-        }
-        const { data } = await connection.post(`/subscripcion`,{data:{...toSave}});
-        res.status(200).json({ message: "ok", action: data });
+            auth: auth,
+            p256dh: p256dh,
+            endpoint: endpoint,
+            expiration_time: expirationTime,
+            usuario_id: usuario_id,
+        };
+        const { saved } = await connection.post(`/subscripcion`, {
+            data: { ...toSave },
+        });
+        res.status(200).json({ message: "ok", action: saved });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: error });
@@ -38,18 +41,19 @@ router.post("/subscribe", async (req, res) => {
 });
 
 router.post("/new-message", async (req, res) => {
-    const parsed_data= JSON.parse(req.body.data);
-    const { message, subscription_id } = parsed_data;
-    const connection = createAxios(baseURL,tenantId) ;
-    const {data} = await connection.get(`/subscripcion/${subscription_id}`);
+    let { data } = req.body;
+    if (typeof data === "undefined") return res.status(400).json({ message: "bad request" });
+    const { message, subscription_id } = parsed_data.data;
+    const connection = createAxios(baseURL, tenantId);
+    const { result } = await connection.get(`/subscripcion/${subscription_id}`);
     const configData = {
-            endpoint:data.endpoint,
-            expirationTime:data.expiration_time,
-            keys:{
-             auth:data.value1,
-             p256dh:data.value2,
-            }
-    }
+        endpoint: result.endpoint,
+        expirationTime: result.expiration_time,
+        keys: {
+            auth: result.auth,
+            p256dh: result.p256dh,
+        },
+    };
     const payload = JSON.stringify({
         title: "Hoy provoca",
         message,
