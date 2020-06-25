9adda71dd0180e04ff0760aa39fae80503463b30
Ruben D. Garcia <rdgp99@gmail.com>
2020-06-25 19:03:56 -0400

    removed axios timeout

--- server/api/src/helpers/axios.ts
+++ server/api/src/helpers/axios.ts
@@ -6,7 +6,6 @@ export const createAxios = function (baseURL: string, tenantId: string) : AxiosI
     const instance : AxiosInstance = axios.create({
         baseURL: baseURL,
         withCredentials: true,
-        timeout: 30000,
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
