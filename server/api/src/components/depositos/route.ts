533eb1e713c92b0dc6fd0b329269893782cc6e0e
Ruben D. Garcia <rdgp99@gmail.com>
2020-06-25 23:40:57 -0400

    fixing issue with undefined fields @/reporteador

--- server/api/src/components/depositos/route.ts
+++ server/api/src/components/depositos/route.ts
@@ -34,6 +34,7 @@ router.get('/:id', validar, async (req: Request, res: Response): Promise<Respons
 //obtener los conceptos de un deposito
 router.get('/:id/conceptos/', validar, async (req: Request, res: Response): Promise<Response> => {
     let { id } = req.params;
+    console.log(req.query)
     try {
         let tenantId: string = getTenantId(req);
         let { message, response, code } = await controller.getConceptosBydeposito(id, req.query, tenantId);
