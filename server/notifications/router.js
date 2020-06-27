8ec9274542032079a97455e58daeb176e3825e08
Ruben D. Garcia <rdgp99@gmail.com>
2020-06-27 01:48:46 -0400

    added physical nots

--- server/notifications/router.js
+++ server/notifications/router.js
@@ -1,9 +1,11 @@
 const mailRouter = require('./controllers/mail/router');
 const pushRouter = require('./controllers/client/push');
+const dbRouter   = require ('./controllers/db/router')
 //const chatRouter = require('./controllers/messages/router');
 
 module.exports = app => {
     app.use('/nots/mail/', mailRouter);
     app.use('/nots/push/', pushRouter);
+    app.use('/nots/db/', dbRouter)
   //  app.use('/nots/chat/', chatRouter);
 }
