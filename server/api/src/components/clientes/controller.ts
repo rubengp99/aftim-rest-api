af2618b13723423af3339dbcd92e1ca546335825
Ruben D. Garcia <rdgp99@gmail.com>
2020-07-06 19:48:05 -0400

    fixed MySQL query for client's dollar contribution

--- server/api/src/components/clientes/controller.ts
+++ server/api/src/components/clientes/controller.ts
@@ -116,7 +116,7 @@ export async function getMostBuyers(query: any, tenantId: string): Promise<any>{
         let sql = `SELECT COUNT(adm_det_facturas.id) AS compras, 
         SUM(adm_det_facturas.precio) AS total, 
         SUM(adm_det_facturas.precio_dolar) AS totalDolar, adm_clientes.* 
-        FROM adm_det_facturas LEFT JOIN adm_clientes ON adm_det_facturas.adm_clientes_id = adm_clientes.id
+        FROM adm_det_facturas LEFT JOIN adm_clientes ON adm_enc_facturas.adm_clientes_id = adm_clientes.id
         LEFT JOIN adm_enc_facturas ON adm_enc_facturas_id = adm_enc_facturas.id
         WHERE adm_enc_facturas.adm_tipos_facturas_id IN (5,1) ${where}
         GROUP BY  adm_enc_facturas.adm_clientes_id ORDER BY totalDolar ${query.order || 'DESC'} LIMIT ${query.limit || '10'}`;
